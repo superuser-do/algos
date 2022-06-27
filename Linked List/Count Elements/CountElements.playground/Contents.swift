6b117f937fea62d5766e2de2f35d9cffdcf6d721
superuser-do <49968227+superuser-do@users.noreply.github.com>
2022-06-26 19:53:22 -0700

    count elements

--- Linked List/Count Elements/CountElements.playground/Contents.swift
+++ Linked List/Count Elements/CountElements.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+import AlgoHelpers
 
-var greeting = "Hello, playground"
+func countElements<T>(head: ListNode<T>?, _ type: ExecutionType) -> Int {
+    switch type {
+    case .iterative:
+        guard var current = head else {
+            return 0
+        }
+        
+        var count = 1
+        while current.next != nil {
+            current = current.next!
+            count += 1
+        }
+        
+        return count
+        
+    case .recursive:
+        guard let head = head else { return 0 }
+        
+        return 1 + countElements(head: head.next, .recursive)
+    }
+}
+
+let LL1 = ListNode(1, ListNode(4, ListNode(5)))
+countElements(head: LL1, .iterative)
+countElements(head: LL1, .recursive)
