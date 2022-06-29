ff6b151e321ec0b39633897ea8825456876dddcc
superuser-do <49968227+superuser-do@users.noreply.github.com>
2022-06-29 00:04:02 -0700

    day 1

---
+++ Daily Coding Problems/Day 1/Day1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func day1(_ nums: [Int], k: Int) -> Bool {
+    var seen = Set<Int>()
+    for num in nums {
+        if seen.contains(k - num) {
+            return true
+        }
+        
+        seen.insert(num)
+    }
+    
+    return false
+}
+
+day1([10, 15, 3, 7], k: 17)
