1eea135867db6187a951bd03b908818ed337d15f
superuser-do <49968227+superuser-do@users.noreply.github.com>
2022-06-30 09:04:18 -0700

    day 5

--- Daily Coding Problems/Day 5/Day5.playground/Contents.swift
+++ Daily Coding Problems/Day 5/Day5.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func cons<L, R>(_ l: L, _ r: R) -> (() -> (L, R)) {
+    return { () -> (L, R) in
+        return (l, r)
+    }
+}
+
+func car<L, R>(_ f: () -> (L, R)) -> L {
+    let (l, _) = f()
+    return l
+}
+
+func cdr<L, R>(_ f: () -> (L, R)) -> R {
+    let (_, r) = f()
+    return r
+}
+
+car(cons(3, 4))
+cdr(cons(3, 4))
