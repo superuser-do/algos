e82d04fadd3b4a16bc0e3491556249d7aa12571d
superuser-do <49968227+superuser-do@users.noreply.github.com>
2022-12-01 15:43:08 -0800

    Parent Sum Tree

--- Trees/Parent Sum Tree/ParentSumTree.playground/Contents.swift
+++ Trees/Parent Sum Tree/ParentSumTree.playground/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+import AlgoHelpers
 
-var greeting = "Hello, playground"
+func immediateParentSum(_ root: TreeNode<Int>?, _ parentValue: Int = 0) -> TreeNode<Int>? {
+    guard let root = root else { return nil }
+
+    immediateParentSum(root.left, root.value)
+    immediateParentSum(root.right, root.value)
+    root.value += parentValue
+    return root
+}
+
+let root = TreeNode(1, TreeNode(2), TreeNode(3))
+immediateParentSum(root)
